f8e7d1ece028a3866a3146924813b2431ec039d8
Maxim <maxcantn@gmail.com>
2025-10-04 20:56:21 +0300

    pic.svg -> vector asset

---
+++ app/src/main/java/com/example/filmoteca/ui/BottomBarUtils.kt
@@ -0,0 +1,13 @@
+package com.example.filmoteca.ui
+
+import android.content.Context
+import androidx.core.content.ContextCompat
+import com.example.filmoteca.R
+import nl.joery.animatedbottombar.AnimatedBottomBar
+
+
+object BottomBarUtils {
+
+}
+
+
