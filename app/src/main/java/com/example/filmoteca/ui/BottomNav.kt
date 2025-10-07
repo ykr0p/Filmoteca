c35c0e6b8ca209ffa1c1028e8b8bfb610cb15008
Maxim <maxcantn@gmail.com>
2025-10-07 14:34:54 +0300

    delete select Toasts in BottomNav.kt

--- app/src/main/java/com/example/filmoteca/ui/BottomNav.kt
+++ app/src/main/java/com/example/filmoteca/ui/BottomNav.kt
@@ -2,7 +2,11 @@
 
 import android.content.Context
 import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.example.filmoteca.R
+
 import nl.joery.animatedbottombar.AnimatedBottomBar
+
 
 fun setupBottomNav(bottomBar: AnimatedBottomBar, context: Context) {
     bottomBar.setOnTabSelectListener(object : AnimatedBottomBar.OnTabSelectListener {
@@ -12,11 +16,7 @@ fun setupBottomNav(bottomBar: AnimatedBottomBar, context: Context) {
             newIndex: Int,
             newTab: AnimatedBottomBar.Tab
         ) {
-            Toast.makeText(context, "Selected: ${newTab.title}", Toast.LENGTH_SHORT).show()
-        }
 
-        override fun onTabReselected(index: Int, tab: AnimatedBottomBar.Tab) {
-            Toast.makeText(context, "Reselected: ${tab.title}", Toast.LENGTH_SHORT).show()
         }
     })
 }
