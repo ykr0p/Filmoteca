c19f9955ae7d3ee1cca799bd69e4fc8798882dc9
Maxim <maxcantn@gmail.com>
2025-10-14 14:48:11 +0300

    configured navigation and add all fragments

--- app/src/main/java/com/example/filmoteca/ui/BottomNav.kt
+++ app/src/main/java/com/example/filmoteca/ui/BottomNav.kt
@@ -1,12 +1,14 @@
 ﻿package com.example.filmoteca.ui
 
 import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import com.example.filmoteca.R
 
 
 import nl.joery.animatedbottombar.AnimatedBottomBar
 
 
-fun setupBottomNav(bottomBar: AnimatedBottomBar, context: Context) {
+fun setupBottomNav(bottomBar: AnimatedBottomBar, activity: AppCompatActivity) {
     bottomBar.setOnTabSelectListener(object : AnimatedBottomBar.OnTabSelectListener {
         override fun onTabSelected(
             lastIndex: Int,
@@ -14,7 +16,16 @@ fun setupBottomNav(bottomBar: AnimatedBottomBar, context: Context) {
             newIndex: Int,
             newTab: AnimatedBottomBar.Tab
         ) {
+            val fragment = when (newTab.id) {
+                R.id.home -> HomeFragment()
+                R.id.explore -> ExploreFragment()
+                R.id.profile -> ProfileFragment()
+                else -> return
+            }
 
+            activity.supportFragmentManager.beginTransaction()
+                .replace(R.id.mainActFrameLayout, fragment)
+                .commit()
         }
     })
 }
--- app/src/main/java/com/example/filmoteca/ui/MainActivity.kt
+++ app/src/main/java/com/example/filmoteca/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.filmoteca.ui
 
+import android.content.Context
 import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
